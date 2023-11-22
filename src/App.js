fc98b5259a9ec1dd2eef51ccd8b6205bf362d0b6
Samir <samir.gasimov.00@gmail.com>
2023-11-23 03:55:08 +0400

    Completed footer-bottom html css

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Main from './components/Main/Main';
 function App() {
   return (
     <div className="App">
-      <div className='Main'>
+      <div className='container'>
         <Header/>
         <Main/>
         <Footer/>
--- src/components/Footer_bottom.js
+++ src/components/Footer_bottom.js
@@ -2,8 +2,24 @@ import React from "react";
 
 export default function Footer_Bottom(){
     return(
-        <div>
-            <h2>This is Footer_botoom component</h2>
+        <div id="footer-bottom">
+            <div className="container">
+                <div className="footer-bottom-main">
+                    <div className="footer-copyright">
+                        <a href="#">copyright 2012 mark anthony</a>
+                    </div>
+                    <div className="footer-bottom-menu">
+                        <ul>
+                            <li><a href="#">about</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li><a href="#">clients</a></li>
+                            <li><a href="#">Practice</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
