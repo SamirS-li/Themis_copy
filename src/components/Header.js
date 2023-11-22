cff296ce13e27b721f7077663357ed340b598db9
Samir <samir.gasimov.00@gmail.com>
2023-11-23 02:31:25 +0400

    Header compleated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,30 @@
 import React from 'react'
+import logo from '../image/header_logo.png'
 
 export default function Header(){
     return(
-        <div>
-            <h2>This is header component</h2>
+        <div id='Header'>
+            <div className="header-container">
+                <div className="header-left">
+                    <div className="header-logo">
+                        <img src={logo} alt="Logo"/>
+                        <a href="#">
+                            <span className='header-title'>mark anthony</span>
+                            <span className="header-subtitle">private lawyer</span>
+                        </a>
+                    </div>
+                    <div className="header-quote">
+                        <h5>Realize your constitutional right</h5>
+                        <h5><span>with qualified help</span></h5>
+                    </div>
+                </div>
+                <div className="header-right">
+                    <div className="header-phone">
+                        <h5>Phone</h5>
+                        <h5><span>1.800.353.252</span></h5>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
