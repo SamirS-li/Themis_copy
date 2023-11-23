835ac573d9e410bc74a3addbebe97d1e155b70f6
SamirS-li <105987045+SamirS-li@users.noreply.github.com>
2023-11-23 04:00:24 +0400

    completed footer bottom with html and css (#2)
    * Header compleated
    
    * Completed footer-bottom html css
    
    ---------
    
    Co-authored-by: Samir <samir.gasimov.00@gmail.com>

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import Main from './components/Main/Main';
 function App() {
   return (
     <div className="App">
-      <div className='Main'>
+      <div className='container'>
+
         <Header/>
         <Main/>
         <Footer/>
