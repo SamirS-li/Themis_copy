8b1c9fd5152b1ecd72fb5a7f414b961b21d3b283
Samir <samir.gasimov.00@gmail.com>
2023-11-28 21:31:14 +0400

    created carousel on Menucontent

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,14 @@
 import React from "react";
 import MainMenu from './MainMenu';
+import MenuContent from './MenuContent'
 // import MenuContent from "./MenuContent";
 
 
 export default function Main(){
     return(
         <div id="Main">
-            <MainMenu/>
-            {/* <MenuContent/> */}
+            {/* <MainMenu/> */}
+            <MenuContent/>
         </div>
     )
 }
--- src/components/Main/MenuContent.js
+++ src/components/Main/MenuContent.js
@@ -1,11 +1,148 @@
 import React from "react";
-import Menus from "./Menus";
+import Slider from "react-slick";
+import '../../../node_modules/slick-carousel/slick/slick.css';
+import '../../../node_modules/slick-carousel/slick/slick-theme.css';
 
-export default function MenuContent(){
+export default class MenuContent extends React.Component {
+    render() {
+        const settings = {
+          dots: false,
+          infinite: true,
+          speed: 650,
+          slidesToShow: 5,
+          slidesToScroll: 1,
+          vertical:true,
+        };
     return(
         <div id="MenuContent">
-            <h2>This is MenuContent component</h2>
+            <div className="menu-content-container">
+                <div className="menu-content-main">
+                    <div className="menu-item-info">
+                     <h2>menu item info</h2>
+
+                    </div>
+                    <div className="menu-content-list">
+                     <div className="menu-content-slider">
+                        <Slider {...settings} className="menu-list">
+                        <li id="about">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="services">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="clients">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="practice">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="contact">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="about">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="services">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="clients">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="practice">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                        <li id="contact">
+                            <a href="#">
+                                <div className="menu-item-content">
+                                        <i></i>
+                                        <div>
+                                            <h2>about</h2>
+                                            <span>mark antony</span>
+                                        </div>
+                                </div>
+                            </a>
+                        </li>
+                    </Slider>
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
             {/* <Menus/> */}
         </div>
-    )
+      )
+    }
 }
