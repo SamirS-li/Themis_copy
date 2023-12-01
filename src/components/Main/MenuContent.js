6b91a040fcb7a2b16defd1e95fad1502e8cac9c9
SamirS-li <105987045+SamirS-li@users.noreply.github.com>
2023-12-02 03:44:10 +0400

    created router (#13)
    * completed footer section without coruesel (#4) (#6)
    
    * Header compleated
    
    * Completed footer-bottom html css
    
    * create legal in Footer
    
    * Completed Footer without corusel
    
    * nothiing : )
    
    ---------
    
    Co-authored-by: Samir <samir.gasimov.00@gmail.com>
    
    * created carousel on Menucontent
    
    * q
    
    * resolve confilict
    
    * bit change slider
    
    * created about page html and css
    
    * App.css file issues
    
    * Created PageTitle component
    
    * Created Servicews page
    
    * Created Clients page
    
    * fix body height issue
    
    * create rout without animation
    
    ---------
    
    Co-authored-by: Samir <samir.gasimov.00@gmail.com>

--- src/components/Main/MenuContent.js
+++ src/components/Main/MenuContent.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Slider from "react-slick";
+import '../../../node_modules/slick-carousel/slick/slick.css';
+import '../../../node_modules/slick-carousel/slick/slick-theme.css';
+import {BrowserRouter as Router,Routes,Route,Link} from "react-router-dom"
 import About from "./Menu/About";
 import Services from "./Menu/Services";
 
 import Clients from "./Menu/Clients";
+import Contact from "./Menu/Contact";
+import Practice from "./Menu/Practice";
 
-
-import '../../../node_modules/slick-carousel/slick/slick.css';
-import '../../../node_modules/slick-carousel/slick/slick-theme.css';
 
 export default class MenuContent extends React.Component {
     render() {
@@ -20,141 +22,115 @@ export default class MenuContent extends React.Component {
           vertical:true,
         };
     return(
-        <div id="MenuContent">
-            <div className="menu-content-container">
-                <div className="menu-content-main">
-                    <div className="menu-item-info">
-                    {/* <About/> */}
 
-                    {/* <Services/> */}
-                    <Clients/>
-
-                    </div>
-                    <div className="menu-content-list">
-                     <div className="menu-content-slider">
-                        <Slider {...settings} className="menu-list">
-                        <li id="about">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>about</h2>
-                                            <span>mark antony</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="services">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-
-                                            <h2>services</h2>
-                                            <span>Legal Services</span>
-
-                                             
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="clients">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>Clients</h2>
-                                            <span>Testimonials</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="practice">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>Practice</h2>
-                                            <span>Practice of Law</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="contact">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>Contact</h2>
-                                            <span>Get In Touch</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="about">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>about</h2>
-                                            <span>mark antony</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="services">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>services</h2>
-                                            <span>Legal Services</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="clients">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>Clients</h2>
-                                            <span>Testimonials</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="practice">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>Practice</h2>
-                                            <span>Practice of Law</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                        <li id="contact">
-                            <a href="#">
-                                <div className="menu-item-content">
-                                        <i></i>
-                                        <div>
-                                            <h2>Contact</h2>
-                                            <span>Get In Touch</span>
-                                        </div>
-                                </div>
-                            </a>
-                        </li>
-                    </Slider>
+        <Router>
+             <div id="MenuContent">
+                 <div className="menu-content-container">
+                     <div className="menu-content-main">
+                         <div className="menu-item-info">
+                         {/* <About/> */}
+                         {/* <Services/> */}
+                         {/* <Clients/> */}
+                          <Routes>
+                            <Route
+                                exact
+                                path="/about"
+                                element={<About />}>
+                            </Route>
+                            <Route
+                                exact
+                                path="/services"
+                                element={<Services />}>
+                            </Route>
+                            <Route
+                                exact
+                                path="/clients"
+                                element={<Clients />}> 
+                            </Route>
+                            <Route
+                                exact
+                                path="/contact"
+                                element={<Contact />}>
+                            </Route>
+                            <Route
+                                exact
+                                path="/practice"
+                                element={<Practice />}> 
+                            </Route>
+                          </Routes>
                         </div>
+                         <div className="menu-content-list">
+                          <div className="menu-content-slider">
+                             <Slider {...settings} className="menu-list">
+                             <li id="about">
+                                 <Link to="/about">
+                                     <div className="menu-item-content">
+                                             <i></i>
+                                             <div>
+                                                 <h2>about</h2>
+                                                 <span>mark antony</span>
+                                             </div>
+                                     </div>
+                                 </Link>
+                             </li>
+                             <li id="services">
+                                 <Link to="/services">
+                                     <div className="menu-item-content">
+                                             <i></i>
+                                             <div>
 
-                    </div>
-                </div>
+                                                 <h2>services</h2>
+                                                 <span>Legal Services</span>
 
-            </div>
-            {/* <Menus/> */}
-        </div>
+
+                                             </div>
+                                     </div>
+                                 </Link>
+                             </li>
+                             <li id="clients">
+                                 <Link to="/clients">
+                                     <div className="menu-item-content">
+                                             <i></i>
+                                             <div>
+                                                 <h2>Clients</h2>
+                                                 <span>Testimonials</span>
+                                             </div>
+                                     </div>
+                                 </Link>
+                             </li>
+                             <li id="practice">
+                                 <Link to="/practice">
+                                     <div className="menu-item-content">
+                                             <i></i>
+                                             <div>
+                                                 <h2>Practice</h2>
+                                                 <span>Practice of Law</span>
+                                             </div>
+                                     </div>
+                                 </Link>
+                             </li>
+                             <li id="contact">
+                                 <Link to="/contact">
+                                     <div className="menu-item-content">
+                                             <i></i>
+                                             <div>
+                                                 <h2>Contact</h2>
+                                                 <span>Get In Touch</span>
+                                             </div>
+                                     </div>
+                                 </Link>
+                             </li>
+                             
+                         </Slider>
+                             </div>
+                             
+                         </div>
+                     </div>
+
+                 </div>
+                 {/* <Menus/> */}
+             </div>
+        </Router>
       )
     }
 }
