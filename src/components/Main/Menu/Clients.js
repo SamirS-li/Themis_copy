e586cc5b799081c225d67e5c21bc75cb5311004a
Samir <samir.gasimov.00@gmail.com>
2023-12-02 02:06:49 +0400

    Created Clients page

--- src/components/Main/Menu/Clients.js
+++ src/components/Main/Menu/Clients.js
@@ -1,9 +1,65 @@
 import React from "react";
+import PageTitle from "./PageTitle"
 
 export default function Clients(){
     return(
-        <div>
-            <h6>This is Clients Contnet component</h6>
+        <div id="clients-page">
+            <PageTitle pageName='Clients'/>
+            <div className="clients-main">
+                <h4 className="clients-title">Clients testimonials</h4>
+                <div className="clients-testimonials">
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                    <div className="testimonials">
+                        <h6 className="testimonial-name">John Carter & Ann Smith</h6>
+                        <p className="testimonial-info">Etiam eleifend tortor at metus interdum ti. 
+                        Suspendisse potenti. Maecenas a odio at lorem elementum molestie mollis a libero.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
