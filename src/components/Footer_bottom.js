449eaa0ec132c0fc9c34b2b81a045703243783af
SamirS-li <105987045+SamirS-li@users.noreply.github.com>
2024-01-24 05:56:56 +0400

    Finished  (#19)
    * added removed css for contact forms
    
    * corrected route when refresh page
    
    * Finish :)
    
    * Ready prod
    
    ---------
    
    Co-authored-by: Samir <samir.gasimov.00@gmail.com>

--- src/components/Footer_bottom.js
+++ src/components/Footer_bottom.js
@@ -1,25 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Footer_Bottom(){
-    return(
-        <div id="footer-bottom">
-            <div className="container">
-                <div className="footer-bottom-main">
-                    <div className="footer-copyright">
-                        <a href="#">copyright 2012 mark anthony</a>
-                    </div>
-                    <div className="footer-bottom-menu">
-                        <ul>
-                            <li><a href="#">about</a></li>
-                            <li><a href="#">Services</a></li>
-                            <li><a href="#">clients</a></li>
-                            <li><a href="#">Practice</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            
+export default function Footer_Bottom() {
+  return (
+    <div id="footer-bottom">
+      <div className="container">
+        <div className="footer-bottom-main">
+          <div className="footer-copyright">
+            <a href="#">copyright 2012 mark anthony</a>
+          </div>
+          <div className="footer-bottom-menu">
+            <ul>
+              <li>
+                <Link to="/menuContent/about">about</Link>
+              </li>
+              <li>
+                <Link to="/menuContent/services">Services</Link>
+              </li>
+              <li>
+                <Link to="/menuContent/clients">clients</Link>
+              </li>
+              <li>
+                <Link to="/menuContent/practice">Practice</Link>
+              </li>
+              <li>
+                <Link to="/menuContent/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
--- src/components/Main/MenuContent.js
+++ src/components/Main/MenuContent.js
@@ -1,235 +1,217 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Slider from "react-slick";
-import '../../../node_modules/slick-carousel/slick/slick.css';
-import '../../../node_modules/slick-carousel/slick/slick-theme.css';
-import {Routes,Route,NavLink, useLocation, useRoutes, useNavigate} from "react-router-dom"
-import {motion} from "framer-motion"
+import "../../../node_modules/slick-carousel/slick/slick.css";
+import "../../../node_modules/slick-carousel/slick/slick-theme.css";
+import {
+  Routes,
+  Route,
+  NavLink
+} from "react-router-dom";
+import { motion } from "framer-motion";
 import About from "./Menu/About";
 import Services from "./Menu/Services";
 import Clients from "./Menu/Clients";
 import Contact from "./Menu/Contact";
 import Practice from "./Menu/Practice";
-import {animationContext} from '../Main/Main'
+import { animationContext } from "../Main/Main";
 
+export default function MenuContent() {
+  const { value, setValue } = useContext(animationContext);
+  const [animationValue, setAnimation] = useState(value);
 
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    vertical: true,
+  };
+  const removeAnimation = () => {
+    setAnimation(false);
 
-export default function MenuContent (){
-   
-          const {value,setValue} = useContext(animationContext)
-          const [animationValue,setAnimation] = useState(value)
-          console.log('animation value: ', animationValue)
+    document
+      .querySelector(".menu-content-main")
+      .classList.remove("animation-item");
+  };
+  useEffect(() => {
+    if (value) {
+      document
+        .querySelector(".menu-content-main")
+        .classList.add("animation-item");
+      setValue(false);
+    }
+  });
 
-    
-        const settings = {
-          dots: false,
-          infinite: true,   
-          speed: 600,
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          vertical:true,
-        };
-        const removeAnimation = ()=>{
-            
-            setAnimation(false);
-            
-            // console.log(document.querySelector('.menu-content-main').classList)
-            document.querySelector('.menu-content-main').classList.remove('animation-item')
-            // console.log(document.querySelector('.menu-content-main').classList)
-        }
-        useEffect(()=>{
-            if(value){
-                console.log('if in ici',value)
-                document.querySelector('.menu-content-main').classList.add('animation-item')
-                setValue(false)
-                setTimeout(()=>{
-                    
-                })
-
-            }
-            
-        })
-        
-        
-        
-    return(
-
-        
-             <div id="MenuContent">
-                 <motion.div 
-                  className="menu-content-container"
-                  initial={{ }}
-                  animate={{}}
-                  exit={{height:"0",transitionDelay:3,delay:"3s"}}
-                  
-                 >
-                     <div className="menu-content-main">
-
-                     
-                         <div className="menu-item-info">
-
-
-
-                         
-                          <Routes>
-                            <Route
-                                exact
-                                path="/about"
-                                element={<About />}>
-                            </Route>
-                            <Route
-                                exact
-                                path="/services"
-                                element={<Services />}>
-                            </Route>
-                            <Route
-                                exact
-                                path="/clients"
-                                element={<Clients />}> 
-                            </Route>
-                            <Route
-                                exact
-                                path="/contact"
-                                element={<Contact />}>
-                            </Route>
-                            <Route
-                                exact
-                                path="/practice"
-                                element={<Practice />}> 
-                            </Route>
-                          </Routes>
-                        </div>
-                         <div className="menu-content-list">
-                            <NavLink to='/' className="content-close-button" onClick={removeAnimation}></NavLink>
-                          <div className="menu-content-slider">
-                             <Slider {...settings} className="menu-small-list">
-                             <li onClick={removeAnimation} id="about-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/about">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>about</h2>
-                                                 <span>mark antony</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="services-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/services">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-
-                                                 <h2>services</h2>
-                                                 <span>Legal Services</span>
-
-
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="clients-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/clients" >
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>Clients</h2>
-                                                 <span>Testimonials</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="practice-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/practice">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>Practice</h2>
-                                                 <span>Practice of Law</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="contact-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/contact">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>Contact</h2>
-                                                 <span>Get In Touch</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="about-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/about">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>about</h2>
-                                                 <span>mark antony</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="services-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/services">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-
-                                                 <h2>services</h2>
-                                                 <span>Legal Services</span>
-
-
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="clients-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/clients" >
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>Clients</h2>
-                                                 <span>Testimonials</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="practice-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/practice">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>Practice</h2>
-                                                 <span>Practice of Law</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                             <li onClick={removeAnimation} id="contact-list">
-                                 <NavLink activeClassName="activeList-a" to="/menuContent/contact">
-                                     <div className="menu-item-content">
-                                             <i></i>
-                                             <div>
-                                                 <h2>Contact</h2>
-                                                 <span>Get In Touch</span>
-                                             </div>
-                                     </div>
-                                 </NavLink>
-                             </li>
-                            
-
-                         </Slider>
-                             </div>
-                             
-                         </div>
-                     </div>
-
-                 </motion.div>
-                 {/* <Menus/> */}
-             </div>
-        
-      )
-    
+  return (
+    <div id="MenuContent">
+      <motion.div
+        className="menu-content-container"
+        exit={{ height: "0", transitionDelay: 3, delay: "3s" }}
+      >
+        <div className="menu-content-main">
+          <div className="menu-item-info">
+            <Routes>
+              <Route exact path="/about" element={<About />}></Route>
+              <Route exact path="/services" element={<Services />}></Route>
+              <Route exact path="/clients" element={<Clients />}></Route>
+              <Route exact path="/contact" element={<Contact />}></Route>
+              <Route exact path="/practice" element={<Practice />}></Route>
+            </Routes>
+          </div>
+          <div className="menu-content-list">
+            <NavLink
+              to="/"
+              className="content-close-button"
+              onClick={removeAnimation}
+            ></NavLink>
+            <div className="menu-content-slider">
+              <Slider {...settings} className="menu-small-list">
+                <li onClick={removeAnimation} id="about-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/about"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>about</h2>
+                        <span>mark antony</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="services-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/services"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>services</h2>
+                        <span>Legal Services</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="clients-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/clients"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>Clients</h2>
+                        <span>Testimonials</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="practice-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/practice"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>Practice</h2>
+                        <span>Practice of Law</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="contact-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/contact"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>Contact</h2>
+                        <span>Get In Touch</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="about-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/about"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>about</h2>
+                        <span>mark antony</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="services-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/services"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>services</h2>
+                        <span>Legal Services</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="clients-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/clients"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>Clients</h2>
+                        <span>Testimonials</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="practice-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/practice"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>Practice</h2>
+                        <span>Practice of Law</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+                <li onClick={removeAnimation} id="contact-list">
+                  <NavLink
+                    activeclassname="activeList-a"
+                    to="/menuContent/contact"
+                  >
+                    <div className="menu-item-content">
+                      <i></i>
+                      <div>
+                        <h2>Contact</h2>
+                        <span>Get In Touch</span>
+                      </div>
+                    </div>
+                  </NavLink>
+                </li>
+              </Slider>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
